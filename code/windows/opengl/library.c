97c22ee04b409ccca95a54794c7a7795c32c343a
feacur <feacur@gmail.com>
2021-03-06 22:36:44 +0300

    setup process requirements

--- code/windows/opengl/library.c
+++ code/windows/opengl/library.c
@@ -545,3 +545,27 @@ static void * rlib_get_function(char const * name) {
 }
 
 #undef OPENGL_CLASS_NAME
+
+//
+
+#if defined(__clang__)
+	#pragma clang diagnostic push
+	#pragma clang diagnostic ignored "-Weverything"
+#endif
+
+// http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+// Global Variable NvOptimusEnablement (new in Driver Release 302)
+// Starting with the Release 302 drivers, application developers can direct the Optimus driver at runtime to use the High Performance Graphics to render any application–even those applications for which there is no existing application profile. They can do this by exporting a global variable named NvOptimusEnablement. The Optimus driver looks for the existence and value of the export. Only the LSB of the DWORD matters at this time. Avalue of 0x00000001 indicates that rendering should be performed using High Performance Graphics. A value of 0x00000000 indicates that this method should beignored.
+__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001UL;
+
+// https://community.amd.com/thread/169965
+// https://community.amd.com/thread/223376
+// https://gpuopen.com/amdpowerxpressrequesthighperformance/
+// Summary
+// Many Gaming and workstation laptops are available with both (1) integrated power saving and (2) discrete high performance graphics devices. Unfortunately, 3D intensive application performance may suffer greatly if the best graphics device is not selected. For example, a game may run at 30 Frames Per Second (FPS) on the integrated GPU rather than the 60 FPS the discrete GPU would enable. As a developer you can easily fix this problem by adding only one line to your executable’s source code:
+// Yes, it’s that easy. This line will ensure that the high-performance graphics device is chosen when running your application.
+__declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001UL;
+
+#if defined(__clang__)
+	#pragma clang diagnostic pop
+#endif
--- code/windows/system.c
+++ code/windows/system.c
@@ -15,9 +15,13 @@ static struct {
 //
 #include "code/platform_system.h"
 
+static void set_process_dpi_awareness(void);
 void platform_system_init(void) {
 	platform_system.module = GetModuleHandleA(NULL);
 	if (platform_system.module == NULL) { fprintf(stderr, "'GetModuleHandle' failed\n"); DEBUG_BREAK(); exit(1); }
+
+	set_process_dpi_awareness();
+
 	timer_to_system_init();
 	window_to_system_init();
 	glibrary_to_system_init();
@@ -48,4 +52,55 @@ void platform_system_sleep(uint32_t millis) {
 
 HMODULE system_to_internal_get_module(void) {
 	return platform_system.module;
+}
+
+//
+
+#include <ShellScalingApi.h>
+
+static void set_process_dpi_awareness(void) {
+#define FREE_DPI_LIBS() \
+	do { \
+		if (User32_dll != NULL) { FreeLibrary(User32_dll); User32_dll = NULL; } \
+		if (Shcore_dll != NULL) { FreeLibrary(Shcore_dll); Shcore_dll = NULL; } \
+	} while (false)\
+
+	HMODULE User32_dll = LoadLibraryA("User32.dll");
+	HMODULE Shcore_dll = LoadLibraryA("Shcore.dll");
+
+	// Windows 10, version 1607
+	if (User32_dll != NULL) {
+		typedef BOOL (WINAPI * set_awareness_func)(DPI_AWARENESS_CONTEXT);
+		set_awareness_func set_awareness = (set_awareness_func)GetProcAddress(User32_dll, "SetProcessDpiAwarenessContext");
+		if (set_awareness != NULL) {
+			set_awareness(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+			FREE_DPI_LIBS();
+		}
+	}
+
+	// Windows 8.1
+	if (Shcore_dll != NULL) {
+		typedef BOOL (WINAPI * set_awareness_func)(PROCESS_DPI_AWARENESS);
+		set_awareness_func set_awareness = (set_awareness_func)GetProcAddress(Shcore_dll, "SetProcessDpiAwareness");
+		if (set_awareness != NULL) {
+			set_awareness(PROCESS_PER_MONITOR_DPI_AWARE);
+			FREE_DPI_LIBS();
+		}
+	}
+
+	// Windows Vista
+	if (User32_dll != NULL) {
+		typedef BOOL (WINAPI * set_awareness_func)(VOID);
+		set_awareness_func set_awareness = (set_awareness_func)GetProcAddress(User32_dll, "SetProcessDPIAware");
+		if (set_awareness != NULL) {
+			set_awareness();
+			FREE_DPI_LIBS();
+		}
+	}
+
+	FREE_DPI_LIBS();
+
+	// https://docs.microsoft.com/windows/win32/hidpi/setting-the-default-dpi-awareness-for-a-process
+
+#undef FREE_DPI_LIBS
 }
