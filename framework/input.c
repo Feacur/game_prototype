ab49260d47f054593465a72acd9889825ebd8164
feacur <feacur@gmail.com>
2021-03-13 02:27:43 +0300

    reuse input state structs

--- framework/input.c
+++ framework/input.c
@@ -10,28 +10,22 @@
 
 struct Keyboard_State {
 	uint8_t keys[KEYBOARD_KEYS_MAX];
-	uint8_t prev[KEYBOARD_KEYS_MAX];
 	// bool caps_lock, num_lock;
 };
 
 struct Mouse_State {
 	uint8_t keys[MOUSE_KEYS_MAX];
-	uint8_t prev[MOUSE_KEYS_MAX];
 
 	uint32_t display_x, display_y;
 	uint32_t window_x, window_y;
-	
-	uint32_t prev_display_x, prev_display_y;
-	uint32_t prev_window_x, prev_window_y;
 
 	int32_t delta_x, delta_y;
-
 	float wheel_x, wheel_y;
 };
 
 struct Input_State {
-	struct Keyboard_State keyboard;
-	struct Mouse_State mouse;
+	struct Keyboard_State keyboard, keyboard_prev;
+	struct Mouse_State mouse, mouse_prev;
 };
 
 static struct Input_State input_state;
@@ -42,7 +36,7 @@ bool input_key(enum Key_Code key) {
 
 bool input_key_transition(enum Key_Code key, bool state) {
 	uint8_t now = input_state.keyboard.keys[key];
-	uint8_t was = input_state.keyboard.prev[key];
+	uint8_t was = input_state.keyboard_prev.keys[key];
 	return (now != was) && (now == state);
 }
 
@@ -67,7 +61,7 @@ bool input_mouse(enum Mouse_Code key) {
 
 bool input_mouse_transition(enum Mouse_Code key, bool state) {
 	uint8_t now = input_state.mouse.keys[key];
-	uint8_t was = input_state.mouse.prev[key];
+	uint8_t was = input_state.mouse_prev.keys[key];
 	return (now != was) && (now == state);
 }
 
@@ -80,24 +74,14 @@ void input_to_platform_reset(void) {
 
 void input_to_platform_before_update(void) {
 	// track input transitions
-	memcpy(input_state.keyboard.prev, input_state.keyboard.keys, sizeof(input_state.keyboard.keys));
-	memcpy(input_state.mouse.prev, input_state.mouse.keys, sizeof(input_state.mouse.keys));
-
-	input_state.mouse.prev_display_x = input_state.mouse.display_x;
-	input_state.mouse.prev_display_y = input_state.mouse.display_y;
-	input_state.mouse.prev_window_x = input_state.mouse.window_x;
-	input_state.mouse.prev_window_y = input_state.mouse.window_y;
+	memcpy(&input_state.keyboard_prev, &input_state.keyboard, sizeof(input_state.keyboard));
+	memcpy(&input_state.mouse_prev, &input_state.mouse, sizeof(input_state.mouse));
 
 	// reset per-frame data
 	input_state.mouse.delta_x = 0;
 	input_state.mouse.delta_y = 0;
 	input_state.mouse.wheel_x = 0;
 	input_state.mouse.wheel_y = 0;
-
-	//
-	input_state.keyboard.keys[KC_SHIFT] = false;
-	input_state.keyboard.keys[KC_CTRL] = false;
-	input_state.keyboard.keys[KC_ALT] = false;
 }
 
 void input_to_platform_after_update(void) {
@@ -112,14 +96,14 @@ void input_to_platform_after_update(void) {
 	memcpy(input_state.keyboard.keys + (uint8_t)'A', input_state.keyboard.keys + (uint8_t)'a', sizeof(*input_state.keyboard.keys) * (1 + 'Z' - 'A'));
 
 	//
-	input_state.keyboard.keys[KC_SHIFT] |= input_state.keyboard.keys[KC_LSHIFT] || input_state.keyboard.keys[KC_RSHIFT];
-	input_state.keyboard.keys[KC_CTRL] |= input_state.keyboard.keys[KC_LCTRL] || input_state.keyboard.keys[KC_RCTRL];
-	input_state.keyboard.keys[KC_ALT] |= input_state.keyboard.keys[KC_LALT] || input_state.keyboard.keys[KC_RALT];
+	input_state.keyboard.keys[KC_SHIFT] = input_state.keyboard.keys[KC_LSHIFT] || input_state.keyboard.keys[KC_RSHIFT];
+	input_state.keyboard.keys[KC_CTRL] = input_state.keyboard.keys[KC_LCTRL] || input_state.keyboard.keys[KC_RCTRL];
+	input_state.keyboard.keys[KC_ALT] = input_state.keyboard.keys[KC_LALT] || input_state.keyboard.keys[KC_RALT];
 
 	//
 	if (input_state.mouse.delta_x == 0 && input_state.mouse.delta_y == 0) {
-		input_state.mouse.delta_x = (int32_t)input_state.mouse.window_x - (int32_t)input_state.mouse.prev_window_x;
-		input_state.mouse.delta_y = (int32_t)input_state.mouse.window_y - (int32_t)input_state.mouse.prev_window_y;
+		input_state.mouse.delta_x = (int32_t)input_state.mouse.window_x - (int32_t)input_state.mouse_prev.window_x;
+		input_state.mouse.delta_y = (int32_t)input_state.mouse.window_y - (int32_t)input_state.mouse_prev.window_y;
 	}
 }
 
