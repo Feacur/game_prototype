bb490085075d08102c0d479a181a135c49a0dbdc
feacur <feacur@gmail.com>
2021-04-14 17:25:42 +0300

    improve build output ux

--- __sublime_project/__user_settings_reference/plugins.py
+++ __sublime_project/__user_settings_reference/plugins.py
@@ -3,6 +3,7 @@ import sublime_plugin
 import re
 import os.path
 
+# %appdata%/Sublime Text 3/Packages/User
 # http://www.sublimetext.com/docs/3/api_reference.html
 
 class FindResultsGotoCommand(sublime_plugin.TextCommand):
@@ -35,4 +36,36 @@ class FindResultsGotoCommand(sublime_plugin.TextCommand):
 		if match:
 			column = view.rowcol(target_range.begin())[1] + 1 - len(match.group(0))
 			return "%s:%s" % (match.group(1), column)
+		return None
+
+class BuildOutputGotoCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+		selection_range = view.sel()[0];
+		file_target = self.get_file_target_clang(selection_range)
+		if file_target is None:
+			file_target = self.get_file_target_msvc(selection_range)
+		if file_target is not None:
+			view.window().open_file(file_target, sublime.ENCODED_POSITION)
+
+	def get_file_target_clang(self, target_range):
+		view = self.view
+		line_range = view.line(target_range)
+		while line_range.begin() > 0:
+			match = re.match(r"^(.+):(\d+:\d+).*$", view.substr(line_range))
+			if match:
+				if os.path.exists(match.group(1)):
+					return "%s:%s" % (match.group(1), match.group(2))
+			line_range = view.line(line_range.begin() - 1)
+		return None
+
+	def get_file_target_msvc(self, target_range):
+		view = self.view
+		line_range = view.line(target_range)
+		while line_range.begin() > 0:
+			match = re.match(r"^(.+)\((\d+),?(\d+)?\).*$", view.substr(line_range))
+			if match:
+				if os.path.exists(match.group(1)):
+					return "%s:%s:%s" % (match.group(1), match.group(2), match.group(3))
+			line_range = view.line(line_range.begin() - 1)
 		return None
