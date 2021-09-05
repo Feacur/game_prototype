925ce29beddf740b34b29ece5b09ad4c435c7052
feacur <feacur@gmail.com>
2021-09-06 02:11:21 +0300

    refactor this init

--- prototype/asset_types.c
+++ prototype/asset_types.c
@@ -122,13 +122,13 @@ void asset_bytes_free(void * instance) {
 static struct Strings asset_json_strings_internal;
 
 void asset_json_type_init(void) {
-	strings_init(&asset_json_strings_internal);
 	asset_json_strings = &asset_json_strings_internal;
+	strings_init(asset_json_strings);
 }
 
 void asset_json_type_free(void) {
+	strings_free(asset_json_strings);
 	asset_json_strings = NULL;
-	strings_free(&asset_json_strings_internal);
 }
 
 void asset_json_init(void * instance, char const * name) {
@@ -138,7 +138,7 @@ void asset_json_init(void * instance, char const * name) {
 	bool const read_success = platform_file_read_entire(name, &buffer);
 	if (!read_success || buffer.count == 0) { DEBUG_BREAK(); }
 
-	json_init(&asset->value, &asset_json_strings_internal, (char const *)buffer.data);
+	json_init(&asset->value, asset_json_strings, (char const *)buffer.data);
 
 	array_byte_free(&buffer);
 }
