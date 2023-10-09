f40e5a6fe9152643f116e6103ed6739c7a4c931b
feacur <feacur@gmail.com>
2023-10-09 02:45:39 +0200

    bikeshed LSP

--- framework/__compilation.h
+++ framework/__compilation.h
@@ -1,15 +1,20 @@
 #if !defined(FRAMEWORK_COMPILATION)
 #define FRAMEWORK_COMPILATION
 
+// ----- ----- ----- ----- -----
+//     saner C99/C11
+// ----- ----- ----- ----- -----
+
 #if defined(__clang__)
-	#pragma clang diagnostic ignored "-Wswitch-enum" // allow partial list of enum cases in switch statements
-	#pragma clang diagnostic ignored "-Wfloat-equal" // allow exact floating point values comparison
-	#pragma clang diagnostic ignored "-Wassign-enum" // allow enum values have implicit flags combinations
-	#pragma clang diagnostic ignored "-Wbad-function-cast" // allow casting function results without temporary assignment
-	#pragma clang diagnostic ignored "-Wdeclaration-after-statement" // allow mixing code and variables declarations
-	#pragma clang diagnostic ignored "-Wunsafe-buffer-usage" // allow unsafe pointer arithmetic
+	// https://clang.llvm.org/docs/DiagnosticsReference.html
+	#pragma clang diagnostic ignored "-Wdeclaration-after-statement" // mixing declarations and code ... C99
+	#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"         // unsafe pointer / unsafe buffer
+	#pragma clang diagnostic ignored "-Wbad-function-cast"           // cast from function call of type A to non-matching type B
+	#pragma clang diagnostic ignored "-Wswitch-enum"                 // enumeration value ... not explicitly handled in switch
+	#pragma clang diagnostic ignored "-Wassign-enum"                 // integer constant not in range of enumerated type A
+	#pragma clang diagnostic ignored "-Wfloat-equal"                 // comparing floating point with == or != is unsafe
 #elif defined(_MSC_VER)
-	#pragma warning(disable : 5105) // macro expansion producing 'defined' has undefined behavior
+	// https://learn.microsoft.com/cpp/error-messages/compiler-warnings/compiler-warnings-c4000-c5999
 	#pragma warning(disable : 4200) // nonstandard extension used : zero-sized array in struct/union
 #endif
 
